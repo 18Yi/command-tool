f075359db5f3186a9ffd2542751d7af472072d90
1301829682 <1301829682@qq.com>
2018-09-13 00:02:42 +0800

    add metalsmith to handle templete

--- bin/macaw-init-templete.js
+++ bin/macaw-init-templete.js
@@ -7,8 +7,10 @@ const program = require('commander')
 const path = require('path')
 const fs = require('fs')
 const glob = require('glob') // npm i glob -D
-const download = require('../lib/download')
-const inquirer = require('inquirer')
+const download = require('../lib/download') // Download templete(下载模板)
+const inquirer = require('inquirer') // Command line interaction(命令行交互)
+const latestVersion = require('latest-version') // get the package latest version
+const generator = require('../lib/generator')
 
 program.usage('<project-name>').parse(process.argv)
 // Get the project name based on input(根据输入，获取项目名称)
@@ -61,12 +63,54 @@ function go() {
   next.then(projectName => {
     if(projectName !== '.'){
       fs.mkdirSync(projectName)
-      download('https://github.com/dzfrontend/react-cli.git#develop', projectName).then(target => {
+      download('https://github.com/dzfrontend/react-cli.git#develop', projectName)
+      .then(target => {
         return {
-          projectName,
+          name: projectName,
+          root: projectName,
           downloadTemp: target
         }
-      }).catch(err => console.log(err))
+      })
+      .then(context => {
+        return inquirer.prompt([
+          {
+            name: 'projectName',
+            message: 'name',
+            default: context.name
+          },
+          {
+            name: 'projectVersion',
+            message: 'version',
+            default: '1.0.0'
+          },
+          {
+            name: 'projectDescription',
+            message: 'description',
+            default: `A project named ${context.name}`
+          }
+        ]).then(answers => {
+          // return latestVersion('macaw-ui').then(version => {
+          //   answers.supportUiVersion = version
+            return {
+              ...context,
+              metadata: {
+                ...answers
+              }
+            }
+          // }).catch(err => {
+          //   return Promise.reject(err)
+          // })
+        })
+      })
+      .then(context => {
+        return generator(context.metadata, context.downloadTemp, path.parse(context.downloadTemp).dir);
+      })
+      .then(context => {
+        console.log('found success')
+      })
+      .catch(err => {
+        console.error(`found faild：${err.message}`)
+      })
     }
   })
 
