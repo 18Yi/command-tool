442cfaf978e5ecb8148fe46f43baf2c68a5bb786
1301829682 <1301829682@qq.com>
2018-09-15 00:53:32 +0800

    fix -g installed then use command bug

--- bin/command.js
+++ bin/command.js
@@ -7,11 +7,11 @@ const program = require('commander')
 program.version('1.0.0')
     .usage('<command> [project-name]')
     .command('hello', 'this will run command-hello.js') // Git-style sub-commands
-    .command('init', 'this will run command-init.js') // Git-style sub-commands
-    .command('init-templete', 'this will run command-init-templete.js')
+    .command('test', 'this will run command-test.js') // Git-style sub-commands
+    .command('init', 'this will run command-init.js')
     .parse(process.argv)
 
 // test case
 // node ./bin/command.js hello
-// node ./bin/command.js init project
-// node ./bin/command.js init-templete my-project
+// node ./bin/command.js test my-project
+// node ./bin/command.js init my-project
